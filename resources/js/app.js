7bbcb838c9e19716ac480e29c869d93f3cd1f9d0
Alpha <alphasteve410@gmail.com>
2023-02-20 12:26:36 +0100

    init

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+let route = document.getElementById('root');
+let root = ReactDOM.createRoot(route);
+
+root.render(
+    <h1>Bonjour</h1>
+)
